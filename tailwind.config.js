01b7756895a295875d59caf97b214b686b9f4078
Den4200 <dennis@dennispham.me>
2022-11-06 19:12:47 -0500

    Extend tailwind theme with Merck's teal color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        merck: {
+          teal: "#007a73",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
