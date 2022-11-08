b0eb2215387d9e8991d4afc52ff67a1ad69f721b
Den4200 <dennis@dennispham.me>
2022-11-08 14:04:14 -0500

    Add Silicon API wrapper functions.

---
+++ utils/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const ApiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
+
+export enum SearchType {
+  casNumber = "cas_number",
+  productName = "product_name",
+  productNumber = "product_number",
+}
+
+export type SafetyDataSheet = {
+  id: number;
+  product_name: string;
+  product_brand: string;
+  product_number: string;
+  cas_number: string;
+  pdf_download_url: string;
+  data: any;
+}
+
+const searchSds = async (query: string, type: SearchType): Promise<SafetyDataSheet[]> => {
+  const res = await ApiClient.get("/sds/search", { params: { [type]: query } })
+  return res.data as SafetyDataSheet[];
+};
+
+export { searchSds };
